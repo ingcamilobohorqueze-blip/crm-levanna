234875f12dd6d67a966a2241909b100ebdc2a54b
Camilo <ing.camilobohorqueze@gmail.com>
2026-06-22 12:02:05 -0500

    fix: update pwa icons for android visibility

--- resize.ps1
+++ resize.ps1
@@ -1,8 +1,11 @@
 Add-Type -AssemblyName System.Drawing
-$src = [System.Drawing.Image]::FromFile('public\logo.png')
+$src = [System.Drawing.Image]::FromFile('public\logo-white.png')
+
+$bgColor = [System.Drawing.Color]::FromArgb(255, 8, 6, 13)
+
 $bmp1 = New-Object System.Drawing.Bitmap 192, 192
 $g1 = [System.Drawing.Graphics]::FromImage($bmp1)
-$g1.Clear([System.Drawing.Color]::Transparent)
+$g1.Clear($bgColor)
 $scale1 = 150.0 / [math]::Max($src.Width, $src.Height)
 $newW1 = [int]($src.Width * $scale1)
 $newH1 = [int]($src.Height * $scale1)
@@ -15,7 +18,7 @@ $bmp1.Dispose()
 
 $bmp2 = New-Object System.Drawing.Bitmap 512, 512
 $g2 = [System.Drawing.Graphics]::FromImage($bmp2)
-$g2.Clear([System.Drawing.Color]::Transparent)
+$g2.Clear($bgColor)
 $scale2 = 400.0 / [math]::Max($src.Width, $src.Height)
 $newW2 = [int]($src.Width * $scale2)
 $newH2 = [int]($src.Height * $scale2)
